653cc864c593911a4fafd1eab801c5e8ab94681b
hyisfine <1257031865@qq.com>
2021-09-16 18:59:33 +0800

    update

--- golang/point/main.go
+++ golang/point/main.go
@@ -2,51 +2,18 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 	"time"
 )
 
-type V struct {
-	Name string `json:"name,omitempty"`
-}
+var c = 1
 
-type Vertex struct {
-	X int
-	Y int
-}
-
-type Scale interface {
-	scale(int)
+func println() {
+	c++
+	fmt.Println(c)
 }
 
 func main() {
-
-	vv := &Vertex{1, 2}
-	var vs interface{} = vv
-	fmt.Println(vs)
-
-	hosts := map[string]IPAddr{
-		"loopback":  {127, 0, 0, 1},
-		"googleDNS": {8, 8, 8, 8},
-	}
-	for name, ip := range hosts {
-		fmt.Printf("%v: %v\n", name, ip)
-	}
-}
-
-func (v *Vertex) scale(n int) {
-	v.X = v.X * n
-	v.Y = v.Y * n
-}
-
-type IPAddr [4]byte
-
-func (ia IPAddr) String() string {
-	ss := make([]string, len(ia))
-	for i, v := range ia {
-		ss[i] = strconv.Itoa(int(v))
-	}
-	time.Sleep(time.Millisecond * 2)
-	return strings.Join(ss, ",")
+	go println()
+	time.Sleep(time.Second)
+	println()
 }
