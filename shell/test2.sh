7e89414e3a9c0660a27380b06a6dcd1348503e87
hyisfine <1257031865@qq.com>
2021-09-22 22:15:23 +0800

    shell:21-9-22

--- shell/test2.sh
+++ shell/test2.sh
@@ -1,1 +1,18 @@
 echo test source file
+
+getopts 'ab:c' op
+echo $op
+echo $OPTARG
+echo $OPTIND
+getopts 'ab:c' op
+echo $op
+echo $OPTARG
+echo $OPTIND
+getopts 'ab:c' op
+echo $op
+echo $OPTARG
+echo $OPTIND
+
+arg='-l'
+
+ls -- $arg
