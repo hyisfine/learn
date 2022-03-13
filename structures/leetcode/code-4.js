f0ed7a5d252c4aeea04bd08fc12adb9d773e7815
hyisfine <1257031865@qq.com>
2022-03-13 18:03:16 +0800

    code:22-3-13

--- structures/leetcode/code-4.js
+++ structures/leetcode/code-4.js
@@ -1,3 +1,8 @@
+function ListNode(val, next) {
+	this.val = val === undefined ? 0 : val
+	this.next = next === undefined ? null : next
+}
+
 /**
  * @param {number[]} nums
  * @return {number[][]}
@@ -166,4 +171,129 @@ var minCostClimbingStairs = function (cost) {
 		dp0 = temp
 	}
 	return dp1
+}
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function (nums, k) {
+	let map = new Map()
+	map.set(0, 1)
+	let count = 0
+	for (let i = 0; i < nums.length; i++) {
+		nums[i] = (nums[i - 1] || 0) + nums[i]
+		if (map.has(nums[i] - k)) count += map.get(nums[i] - k)
+		map.set(nums[i], (map.get(nums[i]) || 0) + 1)
+	}
+	return count
+}
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+	let l1 = headA
+	let l2 = headB
+	while (true) {
+		if (l1 && l1 === l2) return l1
+		l1 = l1?.next
+		l2 = l2?.next
+		if (!l1 && !l2) return null
+		if (!l1) l1 = headB
+		if (!l2) l2 = headA
+	}
+}
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function (nums) {
+	let len = nums.length
+	if (!len) return 0
+	if (len === 1) return nums[0]
+	let dp = Array(nums.length).fill(0)
+	dp[0] = nums[0]
+	dp[1] = Math.max(nums[0], nums[1])
+	for (let i = 2; i < len - 1; i++) dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i])
+	let result1 = dp[len - 2]
+	dp[1] = nums[1]
+	dp[2] = Math.max(nums[1], nums[2])
+	for (let i = 3; i < len; i++) dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i])
+	let result2 = dp[len - 1]
+	return Math.max(result1, result2)
+}
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+	let arr1 = []
+	let arr2 = []
+	while (l1) {
+		arr1.unshift(l1.val)
+		l1 = l1.next
+	}
+	while (l2) {
+		arr2.unshift(l2.val)
+		l2 = l2.next
+	}
+	let arr = []
+	let ans = 0
+	while (arr1.length || arr2.length) {
+		let val = (arr1.shift() || 0) + (arr2.shift() || 0) + ans
+		ans = Math.floor(val / 10)
+		val %= 10
+		arr.unshift(val)
+	}
+	if (ans) arr.unshift(arr)
+	let l = new ListNode()
+	let head = l
+	for (const val of arr) {
+		l.next = new ListNode(val)
+		l = l.next
+	}
+	return head.next
+}
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function (head) {
+	if (!head || !head.next) return head
+	let slow = head
+	let fast = head.next
+	while (fast && fast.next) {
+		slow = slow.next
+		fast = fast.next.next
+	}
+	let next = slow.next
+	slow.next = null
+
+	next = sortList(next)
+	head = sortList(head)
+
+	let l = new ListNode()
+	let h = l
+
+	while (head && next) {
+		if (head.val <= next.val) {
+			l.next = head
+			head = head.next
+		} else {
+			l.next = next
+			next = next.next
+		}
+		l = l.next
+	}
+
+	l.next = head || next
+	return h.next
 }
